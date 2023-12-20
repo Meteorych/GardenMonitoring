89f1e0d2b04b7b21f75d6e9540abf7b2882acbd8
Meteorych <super.titlov@inbox.ru>
2023-12-21 02:20:01 +0300

    Helper window

--- Controllers/PlantsController.cs
+++ Controllers/PlantsController.cs
@@ -38,96 +38,7 @@ namespace GardenMonitoring.Controllers
             return View(plant);
         }
 
-        // GET: Plants/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Plants/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,ClassId")] Plant plant)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(plant);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(plant);
-        }
-
-        // GET: Plants/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var plant = await _context.Plant.FindAsync(id);
-            if (plant == null)
-            {
-                return NotFound();
-            }
-            return View(plant);
-        }
-
-        // POST: Plants/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ClassId")] Plant plant)
-        {
-            if (id != plant.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(plant);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PlantExists(plant.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(plant);
-        }
-
-        // GET: Plants/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var plant = await _context.Plant
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (plant == null)
-            {
-                return NotFound();
-            }
-
-            return View(plant);
-        }
-
+        
         // POST: Plants/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -141,6 +52,19 @@ namespace GardenMonitoring.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        
+        public async Task<IActionResult> Helper(string? name)
+        {
+			if (name == null)
+			{
+				return View();
+			}
+
+			var plant = await _context.Plant
+				.FirstOrDefaultAsync(plant => plant.Name == name);
+			return plant == null ? View() : View("Helper", plant);
         }
 
         private bool PlantExists(int id)
